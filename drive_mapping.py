c22235431b5a7a6c4b734748ab688959e841138f
Lixing Liang <lianglx.alex@gmail.com>
2019-11-27 22:18:02 +0800

    Update and rename  drive_mapping.py to drive_mapping.py

---
+++ drive_mapping.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 27 22:01:16 2019
+
+@author: Alex
+"""
+
+import os
+
+# delete drive
+os.system(r"net use Q: /del")
+
+# map drive
+os.system(r"net use Q: \\server_path\folder_name")
+
+import subprocess
+
+# Disconnect anything on M
+subprocess.call(r'net use m: /del', shell=True)
+
+# Connect to shared drive, use drive letter M
+subprocess.call(r'net use m: \\shared\folder /user:user123 password', shell=True)
