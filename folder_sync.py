a429a6eae746937d10f601428c814a9f35dc4e11
Lixing Liang <lianglx.alex@gmail.com>
2019-11-29 15:15:34 +0800

    Update folder_sync.py

--- folder_sync.py
+++ folder_sync.py
@@ -8,6 +8,8 @@ Created on Wed Nov 27 22:09:28 2019
 
 import os
 from distutils import dir_util
+from datetime import datetime
+import glob
 
 # directory paths
 path_1 = r""
@@ -26,8 +28,18 @@ if confirmation != "Y":
 print("Synchronizing...")
 
 # compare modified datetimes of folders
-modified_time_1 = os.path.getmtime(path_1)
-modified_time_2 = os.path.getmtime(path_2)
+modified_1 = glob.glob(path_1 + r"\**", recursive=True)
+list_1_file = max(modified_1, key=os.path.getmtime)
+modified_2 = glob.glob(path_2 + r"\**", recursive=True)
+list_2_file = max(modified_2, key=os.path.getmtime)
+
+modified_time_1 = os.path.getmtime(list_1_file)
+print("Path 1: " + list_1_file)
+print("Path 1 Modified Time: " + datetime.fromtimestamp(modified_time_1).strftime("%Y.%m.%d %H:%M:%S"))
+
+modified_time_2 = os.path.getmtime(list_2_file)
+print("Path 2: " + list_2_file)
+print("Path 2 Modified Time: " + datetime.fromtimestamp(modified_time_2).strftime("%Y.%m.%d %H:%M:%S"))
 
 list_1 = os.listdir(path_1)
 list_2 = os.listdir(path_2)
@@ -47,4 +59,7 @@ elif len(list_2):
 
 # overwrite old folder with new one
 if src_path != "":
+	print("Copying...")
+	print("From : " + src_path)
+	print("To : " + dest_path)
 	dir_util.copy_tree(src_path, dest_path)
